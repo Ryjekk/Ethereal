762a9e802ecedccda781577363e2bdff767967e3
Inuk Ryjewsky <ryjewski@gmail.com>
2020-08-09 23:25:50 +0200

    adding paralax option

--- src/Components/WelcomePageParallax/style.js
+++ src/Components/WelcomePageParallax/style.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 import {COLORS, QUERIES, SmallHeading} from '../../Style';
 
+import bg from '../../Assets/skullPink.jpg'
+// TODO if you want paralax
+
 export const WelcomeCont = styled.div`
     background-color: ${COLORS.black};
     width: 100vw;
     height: 100vh;
     padding: 100px 0 50px 0;
+    
+    // TODO if you want paralax
+    // background-image: url(${bg});
+    // background-attachment: fixed;
+    // background-position: center;
+    // background-repeat: no-repeat;
+    // background-size: cover;
    
     @media (${QUERIES.medium}) {
         padding: 100px 0;
