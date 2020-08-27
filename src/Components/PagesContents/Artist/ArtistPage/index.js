a8cae364a28ec03bbf482b314693d1d8a199b81a
Inuk Ryjewsky <ryjewski@gmail.com>
2020-08-27 20:50:03 +0200

    adding btn

--- src/Components/PagesContents/Artist/ArtistPage/index.js
+++ src/Components/PagesContents/Artist/ArtistPage/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ArtistHead from "../../../Contents/DivideComponent";
+import {Link} from "react-router-dom";
+import {OptionsLink} from "../../NotFoundPage/style";
+import DivBtn from "../../../UI/DivBtn";
+import {ArtBtnWrap} from "./style";
 
 const ArtistPage = () => {
     return (
         <div>
             <ArtistHead text={'Anna'}/>
+            <ArtBtnWrap>
+                <DivBtn>
+                    <Link to='/booking'>
+                        <OptionsLink>
+                            Make an appointment
+                        </OptionsLink>
+                    </Link>
+                </DivBtn>
+            </ArtBtnWrap>
         </div>
     )
 }
--- src/Components/PagesContents/Artist/ArtistPage/style.js
+++ src/Components/PagesContents/Artist/ArtistPage/style.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+import {ArtistButtonWrapper} from "../ArtistContent/style";
+
+// TODO make universal parring 34px ?
+
+export const ArtBtnWrap = styled(ArtistButtonWrapper)`
+    justify-content: center;
+    padding-top: 34px;
+`;
