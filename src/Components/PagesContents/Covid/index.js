ba3c92d64684d1627b6395f136f3ec17a441e2e7
Inuk Ryjewsky <ryjewski@gmail.com>
2020-10-22 19:51:44 +0200

    fix COVID mobile

--- src/Components/PagesContents/Covid/index.js
+++ src/Components/PagesContents/Covid/index.js
@@ -3,13 +3,14 @@ import {
     WelcomeBox,
     WelcomeCont,
     WelcomeHeading,
-    WelcomeParagraph
+    WelcomeParagraph, WelcomeParagraphSmall
 } from "../WelcomePage/WelcomePageParallax/style";
 
 const CovidPage = () => {
     return (
         <WelcomeCont>
             <WelcomeBox>
+                {/*TODO DO IT BETTER*/}
                 <WelcomeHeading>COVID-19 UPDATE</WelcomeHeading>
                 <WelcomeParagraph>
                     Due to COVID-19, we ask that all of our clients will follow the rules:
@@ -28,6 +29,21 @@ const CovidPage = () => {
                     <br/>
                     For more questions please call as at (+354) 793 2134 or send us email: aura.reykjavik@gmail.com
                 </WelcomeParagraph>
+                <WelcomeParagraphSmall>
+                    Due to COVID-19, we ask that all of our clients will follow the rules:
+                    <br/>
+                    Be on time for your appointment. Not earlier, not later.
+                    <br/>
+                    Wash your hands thoroughly and use sanitizer upon arrival.
+                    <br/>
+                    Medical mask must be on at all times.
+                    <br/>
+                    No tattoos that require mask removal.
+                    <br/>
+                    Moral support not be accommodated, no groups.
+                    <br/>
+                    If you are feeling ill or feel any illness symptoms, possibly had contact with person with covid, please reschedule your appointment as soon as possible.
+                </WelcomeParagraphSmall>
             </WelcomeBox>
         </WelcomeCont>
     )
