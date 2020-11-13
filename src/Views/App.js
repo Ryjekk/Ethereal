a132b75d454cac311f800e10f4afb67d814c815f
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-13 17:50:15 +0100

    loading screen

--- src/Views/App.js
+++ src/Views/App.js
@@ -12,34 +12,57 @@ import DivideComponent from "../Components/Contents/DivideComponent";
 // import ComingSoon from "../Components/PagesContents/ComingSoon";
 import { Container, Inner } from '../Style';
 
-function App() {
-  return (
-      <Container>
-          {/*<ComingSoon/>*/}
-          {/* TODO uncoment */}
-          <NavBar/>
-          <Parallax pages={2} className="ClearSearchbar">
-              <ParallaxLayer speed={0.5}>
-                  <WelcomePage />
-              </ParallaxLayer >
-          </Parallax>
-          <WelcomePageParallax/>
-          <DivideComponent text={"OUR CREW"} paragraph={"Book an appointment with one of our artists and get awesome custom-designed tattoo!"}/>
-          <Inner>
-              <ArtistContent/>
-          </Inner>
-          <DivideComponent text={"OUR GUESTS"} paragraph={"Meet Our Guests. (Guests Spots are time-framed events)"}/>
-          <Inner>
-              <GuestsContent/>
-          </Inner>
-          <DivideComponent text={"FAQ"} paragraph={"Frequently Asked Questions"}/>
-          <Inner>
-              <FaqContent />
-              <MapsGoogle/>
-              <FooterContent/>
-          </Inner>
-      </Container>
-  );
+class App extends React.Component {
+    // fake authentication Promise
+    authenticate(){
+        return new Promise(resolve => setTimeout(resolve, 2500)) // 2 seconds
+    }
+
+    componentDidMount(){
+        this.authenticate().then(() => {
+            const el = document.getElementById('ipl-progress-indicator')
+            if(el){
+                // fade out
+                el.classList.add('available')
+                setTimeout(() => {
+                    // remove from DOM
+                    el.outerHTML = ''
+                }, 2000)
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                {/*<ComingSoon/>*/}
+                {/* TODO uncoment */}
+                <NavBar/>
+                <Parallax pages={2} className="ClearSearchbar">
+                    <ParallaxLayer speed={0.5}>
+                        <WelcomePage />
+                    </ParallaxLayer >
+                </Parallax>
+                <WelcomePageParallax/>
+                <DivideComponent text={"OUR CREW"} paragraph={"Book an appointment with one of our artists and get awesome custom-designed tattoo!"}/>
+                <Inner>
+                    <ArtistContent/>
+                </Inner>
+                <DivideComponent text={"OUR GUESTS"} paragraph={"Meet Our Guests. (Guests Spots are time-framed events)"}/>
+                <Inner>
+                    <GuestsContent/>
+                </Inner>
+                <DivideComponent text={"FAQ"} paragraph={"Frequently Asked Questions"}/>
+                <Inner>
+                    <FaqContent />
+                    <MapsGoogle/>
+                    <FooterContent/>
+                </Inner>
+            </Container>
+        );
+    }
+
+
 }
 
 export default App;
