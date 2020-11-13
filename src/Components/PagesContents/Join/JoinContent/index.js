981a8add8a2e3e8535bdb378aa68bce3621f1c2b
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-13 22:24:19 +0100

    join us page

--- src/Components/PagesContents/Join/JoinContent/index.js
+++ src/Components/PagesContents/Join/JoinContent/index.js
@@ -1,11 +1,45 @@
 import React from "react";
-import {Inner} from "../../../../Style";
+import {JoinBox, JoinCont} from "./style";
+import {MediumHeading, Paragraph} from "../../../../Style";
 
 const Join = () => {
     return (
-        <Inner>
-            <p>content</p>
-        </Inner>
+        <JoinCont>
+            <JoinBox>
+                <MediumHeading>Do you want to join our team?</MediumHeading>
+                    <Paragraph>
+                        We are always looking to expand  our team with new artists.
+                    </Paragraph>
+                    <Paragraph>
+                        If you are interested in working at our studio please email us at :
+                        <strong>auraartistswanted@gmail.com</strong>
+                    </Paragraph>
+            </JoinBox>
+            <JoinBox>
+                <MediumHeading>Do you want to jour Guest?</MediumHeading>
+                <Paragraph>
+                    Aura Tattoo Reykjavik has a friendly and helpful staff and we are always happy to accommodate guest artists. We will promote your stay and do our best to keep you as busy as you would like to be while you are here.
+                </Paragraph>
+                <Paragraph>
+                    If you are interested in doing a guest spot, drop us an email* with inquiry to
+                    <strong>auraartistswanted@gmail.com</strong>
+                    and we will get back to you!
+                </Paragraph>
+                <hr/>
+                <Paragraph>
+                    *In the email include such information as:
+                    <ul>
+                        <li>Your name</li>
+                        <li>Current studio</li>
+                        <li>Your social media</li>
+                        <li>Desired date of visit</li>
+                        <li>Why would you like to guest spot at Aura</li>
+                        <li>Do you already have clients in Iceland / Have you been to Iceland before</li>
+                        <li>Tell as a bit about yourself (tattoo style, artistic background, guest experience etc.)</li>
+                    </ul>
+                </Paragraph>
+            </JoinBox>
+        </JoinCont>
     )
 }
 
