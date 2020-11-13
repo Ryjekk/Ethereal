d35fb6ae4075a0898b8bd369316d086f59e78e3b
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-13 22:48:59 +0100

    join page bug fixing

--- src/Components/PagesContents/Join/JoinContent/style.js
+++ src/Components/PagesContents/Join/JoinContent/style.js
@@ -5,11 +5,12 @@ import bg from '../../../../Assets/Backgrounds/bg3.jpg'
 export const JoinCont = styled(ParallaxBox)`
     display: flex;
     flex-direction: column;
+    align-items: stretch;
     width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
     padding: 100px 0 50px 0;
     background-image: url(${bg});
-
+    
     @media (${QUERIES.medium}) {
         padding: 100px 0;
         flex-direction: row;
@@ -19,8 +20,6 @@ export const JoinCont = styled(ParallaxBox)`
 export const JoinBox = styled.div`
      background-color: rgba(12, 12, 12, .7);
      margin: 25px 10px;
-     height: 100%;
-     width: 100%;
      padding: 50px 40px;
     
     @media (${QUERIES.medium}) {
