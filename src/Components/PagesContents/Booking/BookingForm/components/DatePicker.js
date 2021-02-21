28ebc39a54e47319e57cf953cd10250d273f7afb
Inuk Ryjewsky <ryjewski@gmail.com>
2021-02-21 22:37:05 +0100

    debug

--- src/Components/PagesContents/Booking/BookingForm/components/DatePicker.js
+++ src/Components/PagesContents/Booking/BookingForm/components/DatePicker.js
@@ -4,7 +4,7 @@ import {DatePicker as PickerDate} from 'formik-material-ui-pickers';
 
 
 const DatePicker = (props) => {
-    const {label, name, ...rest} = props
+    const {label, name} = props
     return(
         <>
             <Field component={PickerDate} name={name} label={label} style={{marginBottom: "10px"}} />
